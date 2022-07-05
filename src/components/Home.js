2a76638bcfc0190da991735ac4382e933822e3bb
marouane zahaoui <marouanezahaoui835@gmail.com>
2022-07-05 11:16:35 +0100

    get all blogs and styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,21 @@
+import { useState } from "react";
 const Home = () => {
-  const title = "Welcome to my new blog";
+  const [blogs,setBlogs]=useState([
+    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+  ]);
+
   return (
     <div className="content">
-      <h1>{title}</h1>
+      {
+        blogs.map(blog=>(
+          <div className="blog-preview" key={blog.id}>
+            <h2>{blog.title}</h2>
+            <p>Writen By {blog.author}</p>
+          </div>
+        ))
+      }
     </div>
   );
 };
